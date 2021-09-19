f428fdddecaa876ec9ac1d72b425516cdc9ae1b8
KangChanSong <thdrkdcks@naver.com>
2021-09-19 12:49:38 +0900

    CandidateRepositoryTest 수정

--- src/test/java/com/mbtizip/repository/CandidateRepositoryTest.java
+++ src/test/java/com/mbtizip/repository/CandidateRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.mbtizip.repository;
 
+import com.mbtizip.common.enums.TestCategoryEnum;
 import com.mbtizip.common.util.TestEntityGenerator;
 import com.mbtizip.domain.candidate.Candidate;
 import com.mbtizip.domain.candidate.job.Job;
 import com.mbtizip.domain.candidate.person.Person;
 import com.mbtizip.domain.candidate.person.QPerson;
+import com.mbtizip.domain.category.Category;
 import com.mbtizip.domain.common.pageSortFilter.Page;
 import com.mbtizip.repository.candidate.CandidateRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +17,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
+import static com.mbtizip.common.enums.TestCategoryEnum.CATEGORY_NAME_1;
+import static com.mbtizip.common.util.TestEntityGenerator.createCategory;
 import static com.mbtizip.common.util.TestEntityGenerator.createPerson;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,34 +30,35 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CandidateRepositoryTest {
 
     @Autowired
+    EntityManager em;
+
+    @Autowired
     CandidateRepository repository;
+
+    Category category;
+
+    @BeforeEach
+    public void setup(){
+        Category createdCategory = createCategory(CATEGORY_NAME_1);
+        em.persist(createdCategory);
+        category = em.find(Category.class, createdCategory.getId());
+    }
 
     @DisplayName("Person 이 등록된다")
     @Test
     public void Candidate_등록(){
 
         //given
-        Candidate candidate = createPerson();
+        Person person = createPerson();
         //when
-        Long id = repository.save(candidate);
+        person.setCategory(category);
+
+        Long id = repository.save(person);
+        Person found = (Person) repository.find(id);
         //then
-        assertEquals(id, candidate.getId());
+        assertEquals(id, found.getId());
     }
 
-    @DisplayName("Person 이 조회된다")
-    @Test
-    public void Candidate_조회(){
-
-        //given
-        Candidate candidate = createPerson();
-        //when
-        Long id = repository.save(candidate);
-
-        //then
-        Candidate found = repository.find(id);
-        assertEquals(candidate, found);
-        assertEquals(candidate.getId(), found.getId());
-    }
 
     // 목록 조회
     @DisplayName("Person 에 대한 목록이 조회된다.")
@@ -145,8 +152,11 @@ public class CandidateRepositoryTest {
         Person person1 = createPerson(name + "1");
         Person person2 = createPerson(name + "2");
         Person person3 = createPerson(name +"3");
-
         //when
+        person1.setCategory(category);
+        person2.setCategory(category);
+        person3.setCategory(category);
+
         repository.save(person1);
         repository.save(person2);
         repository.save(person3);
